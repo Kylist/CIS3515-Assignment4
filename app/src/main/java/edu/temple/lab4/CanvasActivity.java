b42697573b7c8e99b2fd7756f9addbe611301eab
Kylist <quocanhhnams1316@gmail.com>
2020-10-08 18:34:49 -0400

    lab 5

--- app/src/main/java/edu/temple/lab4/CanvasActivity.java
+++ app/src/main/java/edu/temple/lab4/CanvasActivity.java
@@ -2,12 +2,15 @@ package edu.temple.lab4;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.content.res.Resources;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.support.constraint.ConstraintLayout;
 import android.widget.TextView;
 
 public class CanvasActivity extends Activity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,11 +19,11 @@ public class CanvasActivity extends Activity {
 
         final Intent receivedIntent = getIntent(); //intent from activity 1 gets passed to activity 2. it must be retrieved like this
 
-        String color = receivedIntent.getStringExtra("message"); //takes message from activity 1
+    String color = receivedIntent.getStringExtra("message"); //takes message from activity 1
 
         final ConstraintLayout canvasConstraint = (ConstraintLayout) findViewById(R.id.canvasConstraint);
 
-        canvasConstraint.setBackgroundColor(Color.parseColor((color)));
+        canvasConstraint.setBackgroundColor(Color.parseColor(color));
         TextView messageTextView = findViewById(R.id.textView);
         messageTextView.setText(color); //displays message taken from activity 1
     }
